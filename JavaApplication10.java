f7fa4fac9af2c6f5e000f7ff1f48b05aaf7e4fc8
sanduni-E <sanduniue@gmail.com>
2019-02-10 23:16:56 +0530

    Maths-Operations
    Maths-Operations

---
+++ JavaApplication10.java
@@ -0,0 +1,14 @@
+
+public class JavaApplication10 {
+    public static void main(String[] args) {
+        int var1;
+        int var2;
+        var1=10;
+        var2=20;
+        System.out.println("addition is="+(var1+var2));
+        System.out.println("subtraction is"+(var1-var2));
+        System.out.println("multiplication is"+(var1*var2));
+        System.out.println("division is="+(var1/var2));
+        System.out.println("modulus is="+(var1%var2));
+    }
+}
